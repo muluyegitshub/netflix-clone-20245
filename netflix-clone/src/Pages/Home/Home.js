a4c866535f0ee0a75497086525d3d7b7371f17ae
Mulu alemu <muluye2001@gmail.com>
2024-02-20 18:58:24 +0300

    Row

--- netflix-clone/src/Pages/Home/Home.js
+++ netflix-clone/src/Pages/Home/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from '../../Component/Header/Header';
 import Footer from '../../Component/Footer/Footer';
 import Banner from '../../Component/Banner/Banner';
+ import RowList from '../../Component/Rows/RowList/RowList';
 
 const Home = () => {
   return (
     <>
       <Header/>
      < Banner/>
+     <RowList/>
       <Footer/>
      
      
